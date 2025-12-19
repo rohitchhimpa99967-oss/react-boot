93377746f4847694316865956434fcf631ae3bf5
Rohit Kumar <rohitchhimpa99967@gmail.com>
2025-12-19 18:50:30 +0530

    first commit

---
+++ src/LandingPage.jsx
@@ -0,0 +1,13 @@
+
+
+const LandingPage = () => {
+  return (
+    <div className="w-full h-screen bg-[#f0b04a] flex justify-center items-center flex-col">
+     <img src="../src/assets/Images/Logo.png" alt="Not found" width={'300px'} /> 
+     <h2 className="border-b-[1px] border-black">Since 2004</h2> <br />
+     <p className="text-sm">New Delhi | Model Town | TasteMaker007@gmail.com</p>
+    </div>
+  )
+}
+
+export default LandingPage
