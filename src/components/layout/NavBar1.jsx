3af871b6b7789e1ba8558ba00da83ff2f60c1870
Viikas Paliwal <vikaspaliwal22911@gmail.com>
2026-04-20 09:58:53 +0530

    29-04

--- src/components/layout/NavBar1.jsx
+++ src/components/layout/NavBar1.jsx
@@ -22,7 +22,7 @@ export default function NavBar1() {
     { name: "Category", icon: "fa-solid fa-list", path: "/category" },
     { name: "Products", icon: "fa-solid fa-bowl-food", path: "/products" },
     { name: "Orders", icon: "fa-solid fa-barcode", path: "/orders" },
-    // { name: "Sales", icon: "fa-solid fa-receipt", path: "/sales" },
+    { name: "Sales", icon: "fa-solid fa-receipt", path: "/sales" },
     { name: "Bill", icon: "fa-solid fa-receipt", path: "/bill" },
     { name: "Tables", icon: "fa-solid fa-table", path: "/tables" },
   ];
